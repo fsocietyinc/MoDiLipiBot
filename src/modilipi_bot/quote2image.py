10cf5d614a7efe89ffca11624a01c218ace9c75c
Prasad <theprasadchavan@gmail.com>
2026-04-16 12:54:29 +0530

    Move to uv

--- src/modilipi_bot/quote2image.py
+++ src/modilipi_bot/quote2image.py
@@ -1,28 +1,42 @@
-import os
 import base64
+import os
 from io import BytesIO
 
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def convert(quote, fg, image, border_color, font_file=None, font_size=None,width=None,height=None):
+
+def convert(
+    quote,
+    fg,
+    image,
+    border_color,
+    font_file=None,
+    font_size=None,
+    width=None,
+    height=None,
+):
     x1 = width if width else 612
     y1 = height if height else 612
-        
+
     sentence = f"{quote}"
-    
-    quote = ImageFont.truetype(os.path.join(BASE_DIR, 'MoDiLipiBot\\fonts', 'NotoSansModiAdvanced.ttf'), 70, layout_engine=ImageFont.LAYOUT_RAQM)
 
-    img = Image.new("RGB", (x1, y1), color=(255,255,255))
+    quote = ImageFont.truetype(
+        os.path.join(BASE_DIR, "MoDiLipiBot\\fonts", "NotoSansModiAdvanced.ttf"),
+        70,
+        layout_engine=ImageFont.LAYOUT_RAQM,
+    )
 
-    back = Image.open(image, 'r')
+    img = Image.new("RGB", (x1, y1), color=(255, 255, 255))
+
+    back = Image.open(image, "r")
     img_w, img_h = back.size
     bg_w, bg_h = img.size
     offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
-    bback=back.filter(ImageFilter.BLUR)
+    bback = back.filter(ImageFilter.BLUR)
     img.paste(bback, offset)
-    
+
     d = ImageDraw.Draw(img)
 
     sum = 0
@@ -53,10 +67,18 @@ def convert(quote, fg, image, border_color, font_file=None, font_size=None,width
     qx = x1 / 2 - x2 / 2
     qy = y1 / 2 - y2 / 2
 
-    d.text((qx-1, qy-1), fresh_sentence, align="center", font=quote, fill=border_color)
-    d.text((qx+1, qy-1), fresh_sentence, align="center", font=quote, fill=border_color)
-    d.text((qx-1, qy+1), fresh_sentence, align="center", font=quote, fill=border_color)
-    d.text((qx+1, qy+1), fresh_sentence, align="center", font=quote, fill=border_color)
+    d.text(
+        (qx - 1, qy - 1), fresh_sentence, align="center", font=quote, fill=border_color
+    )
+    d.text(
+        (qx + 1, qy - 1), fresh_sentence, align="center", font=quote, fill=border_color
+    )
+    d.text(
+        (qx - 1, qy + 1), fresh_sentence, align="center", font=quote, fill=border_color
+    )
+    d.text(
+        (qx + 1, qy + 1), fresh_sentence, align="center", font=quote, fill=border_color
+    )
 
     d.text((qx, qy), fresh_sentence, align="center", font=quote, fill=fg)
 
@@ -69,4 +91,3 @@ def get_base64(image):
     img.save(buffered, format="PNG")
     img_str = base64.b64encode(buffered.getvalue())
     return img_str.decode()
-
